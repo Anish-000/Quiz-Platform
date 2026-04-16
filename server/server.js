f9dc9cd353eb9d8aec29bd9da644e795ce16622c
Anish Chattopadhyay <anishchatto2002@gmail.com>
2026-04-16 11:43:18 +0530

    Quiz Platform

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('../client'));
+app.use(express.static('client'));
 
 // Test route
 app.get('/', (req, res) => {
