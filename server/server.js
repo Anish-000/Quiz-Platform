9c03acdf009019a2980f931275a0d025f6654a7c
Anish Chattopadhyay <anishchatto2002@gmail.com>
2026-04-19 08:35:26 +0530

    Quiz Platform

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const db = require('./config/db');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.static('client'));
 
